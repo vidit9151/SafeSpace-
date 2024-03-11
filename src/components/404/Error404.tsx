49c0305cf4df9b8f4b386a9d873df32accf43dff
vidit9151 <vidit9151@gmail.com>
2024-03-12 03:12:04 +0530

    completed

--- src/components/404/Error404.tsx
+++ src/components/404/Error404.tsx
@@ -1,5 +1,13 @@
+import err404 from "../../assets/404 error.svg";
 const Error404 = () => {
-  return <div>Error404</div>;
+  return (
+    <div>
+      <h1 className="pt-10 text-center">no such page exists </h1>
+      <div className="flex justify-center">
+        <img className="p-20" src={err404} alt="" />
+      </div>
+    </div>
+  );
 };
 
 export default Error404;
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,7 +4,7 @@ import FooterCopyright from "./FooterCopyright";
 
 const Footer = () => {
   return (
-    <div className="glass p-4 mt-7">
+    <div className="glass p-4 mt-7 relative bottom-0">
       <div className="flex justify-center p-5">
         <img src={Logo} className="w-28" alt="" />
       </div>
