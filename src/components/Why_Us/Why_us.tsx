1db7af0b38abd398fb506df8d33d3b828e28652f
vidit9151 <vidit9151@gmail.com>
2024-03-11 23:06:42 +0530

    why us done

--- src/components/Why_Us/Why_us.tsx
+++ src/components/Why_Us/Why_us.tsx
@@ -1,9 +1,35 @@
+import { Why_Us_Data } from "./why_us.data";
+
 const Why_us = () => {
   return (
     <div className="glass  mt-2 mx-2 text-center px-4 pb-4 ">
       <h1 className="text-2xl md:text-4xl pt-7 pb-4  ">Why Us ?</h1>
-      <div className="grid grid-cols-2 md:grid-cols-5 gap-8 px-5 pt-[2rem] bg-white pb-6 shadow-inner shadow-black  ">
-        <h1></h1>
+      <div className=" px-5 pt-[2rem] bg-white pb-6 shadow-inner shadow-black flex flex-col  ">
+        <div className="grid grid-cols-4 md:grid-cols-4 justify-between pt-6 pb-8">
+          {Why_Us_Data.map(({ id, title, icon }) => (
+            <div className="flex flex-col items-center">
+              <h1 key={id}>
+                <img src={icon} className="max-w-10" alt={title} />
+              </h1>
+              <p className="font-semibold">{title}</p>
+            </div>
+          ))}
+        </div>
+        <hr />
+        <div className="px-10 py-5 bg-blue-300 pb-6 shadow-inner shadow-black">
+          <ul className="list-disc md:list-none font-thin ">
+            <li>Our Room Freshners are infused with Essential oils</li>
+            <br />
+            <li>Our Room Freshners are Toxin free</li>
+            <br />
+            <li>
+              None of our range of handwashes contain harmful chemicals like
+              Parabens , Triclosan ,Phosphates , Formaldehyde
+            </li>
+            <br />
+            <li>Our Room Freshners are water based</li>
+          </ul>
+        </div>
       </div>
     </div>
   );
---
+++ src/components/Why_Us/why_us.data.tsx
@@ -0,0 +1,11 @@
+import EssOil from "../../assets/essential-oil.svg";
+import NonTox from "../../assets/non toxic.svg";
+import Paraben from "../../assets/Paraben and other.svg";
+import WatBas from "../../assets/water-drop-svgrepo-com.svg";
+
+export const Why_Us_Data = [
+  { id: 1, title: "Essential Oils", icon: EssOil },
+  { id: 2, title: "Non-Toxic", icon: NonTox },
+  { id: 3, title: "No Parabens", icon: Paraben },
+  { id: 4, title: "Water Based", icon: WatBas },
+];
