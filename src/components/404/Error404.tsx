41fb51e498f6a873330557eb9f80cd8c22f5e1a0
vidit9151 <vidit9151@gmail.com>
2024-03-12 03:25:21 +0530

    vercel issue

--- src/components/404/Error404.tsx
+++ src/components/404/Error404.tsx
@@ -1,10 +1,10 @@
-import err404 from "../../assets/404 error.svg";
+import error404 from "../../assets/404 error.svg";
 const Error404 = () => {
   return (
     <div>
       <h1 className="pt-10 text-center">no such page exists </h1>
       <div className="flex justify-center">
-        <img className="p-20" src={err404} alt="" />
+        <img className="p-20" src={error404} alt="" />
       </div>
     </div>
   );
